ce093cdcdc5079a94f25aab04ead44e67854a5c2
saeedhub101 <saeid_w@yahoo.com>
2026-09-25 19:12:01 +0300

    fix: complete Windows 0.61 screen capture API migration

--- src-tauri/src/screen.rs
+++ src-tauri/src/screen.rs
@@ -12,36 +12,54 @@ pub struct CaptureResult {
 pub fn capture_desktop() -> Result<CaptureResult, String> {
     use windows::Win32::Foundation::HWND;
     use windows::Win32::Graphics::Gdi::*;
-    use windows::Win32::UI::WindowsAndMessaging::{GetDC, GetDesktopWindow, GetSystemMetrics, ReleaseDC, SM_CXVIRTUALSCREEN, SM_CYVIRTUALSCREEN, SM_XVIRTUALSCREEN, SM_YVIRTUALSCREEN};
+    use windows::Win32::UI::WindowsAndMessaging::{
+        GetDC, GetDesktopWindow, GetSystemMetrics, ReleaseDC,
+        SM_CXVIRTUALSCREEN, SM_CYVIRTUALSCREEN, SM_XVIRTUALSCREEN, SM_YVIRTUALSCREEN,
+    };
 
     unsafe {
         let hwnd: HWND = GetDesktopWindow();
-        let hdc = GetDC(hwnd);
-        if hdc.0 == 0 { return Err("Unable to acquire desktop DC".into()); }
+        let hdc = GetDC(Some(hwnd));
+        if hdc.0.is_null() {
+            return Err("Unable to acquire desktop DC".into());
+        }
 
         let left = GetSystemMetrics(SM_XVIRTUALSCREEN);
         let top = GetSystemMetrics(SM_YVIRTUALSCREEN);
         let width = GetSystemMetrics(SM_CXVIRTUALSCREEN).max(1) as u32;
         let height = GetSystemMetrics(SM_CYVIRTUALSCREEN).max(1) as u32;
 
-        let mem = CreateCompatibleDC(hdc);
-        if mem.0 == 0 {
+        let mem = CreateCompatibleDC(Some(hdc));
+        if mem.0.is_null() {
             let _ = ReleaseDC(Some(hwnd), hdc);
             return Err("Unable to create capture DC".into());
         }
+
         let bitmap = CreateCompatibleBitmap(hdc, width as i32, height as i32);
         if bitmap.0.is_null() {
             let _ = DeleteDC(mem);
-            let _ = ReleaseDC(hwnd, hdc);
+            let _ = ReleaseDC(Some(hwnd), hdc);
             return Err("Unable to create capture bitmap".into());
         }
+
         let old = SelectObject(mem, bitmap.into());
-        let copied = BitBlt(mem, 0, 0, width as i32, height as i32, Some(hdc), left, top, SRCCOPY | CAPTUREBLT).is_ok();
+        let copied = BitBlt(
+            mem,
+            0,
+            0,
+            width as i32,
+            height as i32,
+            Some(hdc),
+            left,
+            top,
+            SRCCOPY | CAPTUREBLT,
+        ).is_ok();
+
         if !copied {
             let _ = SelectObject(mem, old);
             let _ = DeleteObject(bitmap.into());
             let _ = DeleteDC(mem);
-            let _ = ReleaseDC(hwnd, hdc);
+            let _ = ReleaseDC(Some(hwnd), hdc);
             return Err("Screen capture failed".into());
         }
 
@@ -63,19 +81,34 @@ pub fn capture_desktop() -> Result<CaptureResult, String> {
             &mut info,
             DIB_RGB_COLORS,
         );
-        let _ = SelectObject(mem, old);
-        let _ = DeleteObject(bitmap);
-        let _ = DeleteDC(mem);
-        let _ = ReleaseDC(hwnd, hdc);
 
-        if scanlines == 0 { return Err("Unable to read captured pixels".into()); }
+        let _ = SelectObject(mem, old);
+        let _ = DeleteObject(bitmap.into());
+        let _ = DeleteDC(mem);
+        let _ = ReleaseDC(Some(hwnd), hdc);
+
+        if scanlines == 0 {
+            return Err("Unable to read captured pixels".into());
+        }
+
         let rgba = ImageBuffer::<Rgba<u8>, _>::from_raw(width, height, pixels)
             .ok_or_else(|| "Invalid capture buffer".to_string())?;
+
         let mut png = Vec::new();
-        let mut encoder = image::codecs::png::PngEncoder::new(&mut png);
-        image::ImageEncoder::write_image(&mut encoder, rgba.as_raw(), width, height, image::ExtendedColorType::Rgba8)
-            .map_err(|e| e.to_string())?;
-        Ok(CaptureResult { width, height, png_base64: STANDARD.encode(png) })
+        let encoder = image::codecs::png::PngEncoder::new(&mut png);
+        image::ImageEncoder::write_image(
+            encoder,
+            rgba.as_raw(),
+            width,
+            height,
+            image::ExtendedColorType::Rgba8,
+        ).map_err(|e| e.to_string())?;
+
+        Ok(CaptureResult {
+            width,
+            height,
+            png_base64: STANDARD.encode(png),
+        })
     }
 }
 
